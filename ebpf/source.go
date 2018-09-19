615a11be8060288798a6c2cb6952566eaa389e15
Lorenzo Fontana <lo@linux.com>
2018-09-19 23:36:19 +0200

    Use the sources stuff
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

---
+++ ebpf/source.go
@@ -0,0 +1,160 @@
+package generate
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/influxdata/flux"
+	"github.com/influxdata/flux/execute"
+	"github.com/influxdata/flux/values"
+
+	bpf "github.com/iovisor/gobpf/bcc"
+)
+
+type Source struct {
+	called bool
+	File   string
+	alloc  *execute.Allocator
+}
+
+func NewSource(a *execute.Allocator) *Source {
+	return &Source{alloc: a}
+}
+
+func (s *Source) Connect() error {
+	return nil
+}
+
+func (s *Source) Fetch() (bool, error) {
+	return !s.called, nil
+}
+
+func (s *Source) Decode() (flux.Table, error) {
+	defer func() {
+		s.called = true
+	}()
+	ks := []flux.ColMeta{
+		flux.ColMeta{
+			Label: "_file",
+			Type:  flux.TString,
+		},
+	}
+	vs := []values.Value{
+		values.NewStringValue(s.File),
+	}
+	groupKey := execute.NewGroupKey(ks, vs)
+	b := execute.NewColListTableBuilder(groupKey, s.alloc)
+
+	cols := []flux.ColMeta{
+		flux.ColMeta{
+			Label: "_time",
+			Type:  flux.TTime,
+		},
+		flux.ColMeta{
+			Label: "_value",
+			Type:  flux.TString,
+		},
+	}
+
+	for _, col := range cols {
+		b.AddCol(col)
+	}
+
+	cols = b.Cols()
+
+	colIndex := map[string]int{}
+	for i, col := range cols {
+		colIndex[col.Label] = i
+	}
+
+	buildBpfResult(b)
+
+	return b.Table()
+}
+
+// temporary program here
+const source string = `
+	#include <uapi/linux/ptrace.h>
+	
+	struct readline_event_t {
+	        u32 pid;
+	        char str[80];
+	} __attribute__((packed));
+	
+	BPF_PERF_OUTPUT(readline_events);
+	
+	int get_return_value(struct pt_regs *ctx) {
+	        struct readline_event_t event = {};
+	        u32 pid;
+	        if (!PT_REGS_RC(ctx))
+	                return 0;
+	        pid = bpf_get_current_pid_tgid();
+	        event.pid = pid;
+	        bpf_probe_read(&event.str, sizeof(event.str), (void *)PT_REGS_RC(ctx));
+	        readline_events.perf_submit(ctx, &event, sizeof(event));
+	
+	        return 0;
+	}
+	`
+
+type readlineEvent struct {
+	Pid uint32
+	Str [80]byte
+}
+
+func buildBpfResult(b *execute.ColListTableBuilder) {
+	m := bpf.NewModule(source, []string{})
+	defer m.Close()
+
+	readlineUretprobe, err := m.LoadUprobe("get_return_value")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load get_return_value: %s\n", err)
+		os.Exit(1)
+	}
+
+	err = m.AttachUretprobe("/bin/bash", "readline", readlineUretprobe, -1)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to attach return_value: %s\n", err)
+		os.Exit(1)
+	}
+
+	table := bpf.NewTable(m.TableId("readline_events"), m)
+
+	channel := make(chan []byte)
+
+	perfMap, err := bpf.InitPerfMap(table, channel)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to init perf map: %s\n", err)
+		os.Exit(1)
+	}
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+
+	fmt.Printf("%10s\t%s\n", "PID", "COMMAND")
+	go func() {
+		var event readlineEvent
+		for {
+			data := <-channel
+			err := binary.Read(bytes.NewBuffer(data), binary.LittleEndian, &event)
+			if err != nil {
+				fmt.Printf("failed to decode received data: %s\n", err)
+				continue
+			}
+			// Convert C string (null-terminated) to Go string
+			comm := string(event.Str[:bytes.IndexByte(event.Str[:], 0)])
+
+			b.AppendTime(colIndex["_time"], values.ConvertTime(time.Now())))
+			b.AppendInt(colIndex["_value"], comm)
+
+		}
+	}()
+
+	perfMap.Start()
+	<-time.After(time.Second*10)
+	perfMap.Stop()
+}
+
---
+++ functions/from_ebpf.go
@@ -0,0 +1,170 @@
+package functions
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/influxdata/flux"
+	"github.com/influxdata/flux/ebpf"
+	"github.com/influxdata/flux/execute"
+	"github.com/influxdata/flux/plan"
+	"github.com/influxdata/flux/semantic"
+)
+
+const FromEbpfOpSpec = "fromEbpf"
+
+type FromEbpfOpSpec struct {
+	File string
+}
+
+var fromEbpfSignature = semantic.FunctionSignature{
+	Params: map[string]semantic.Type{
+		"file": semantic.String,
+	},
+	ReturnType: flux.TableObjectType,
+}
+
+func init() {
+	flux.RegisterFunction(FromEbpfkind, createFromEbpfOpSpec, fromEbpfSignature)
+	flux.RegisterOpSpec(fromEbpfKind, newFromEbpfOp)
+	plan.RegisterProcedureSpec(fromEbpfKind, newFromEbpfProcedure, fromEbpfKind)
+	execute.RegisterSource(fromEbpfKind, createFromEbpfSource)
+}
+
+func createFromEbpfOpSpec(args flux.Arguments, a *flux.Administration) (flux.OperationSpec, error) {
+	spec := new(FromEbpfOpSpec)
+
+	if t, ok, err := args.GetString("file"); err != nil {
+		return nil, err
+	}
+
+	return spec, nil
+}
+
+func newFromEbpfOp() flux.OperationSpec {
+	return new(FromEbpfOpSpec)
+}
+
+func (s *FromEbpfOpSpec) Kind() flux.OperationKind {
+	return fromEbpfKind
+}
+
+type FromEbpfProcedureSpec struct {
+	File string
+}
+
+func newFromEbpfProcedure(qs flux.OperationSpec, pa plan.Administration) (plan.ProcedureSpec, error) {
+	// TODO: copy over data from the OpSpec to the ProcedureSpec
+	spec, ok := qs.(*FromEbpfOpSpec)
+	if !ok {
+		return nil, fmt.Errorf("invalid spec type %T", qs)
+	}
+
+	return &FromEbpfProcedureSpec{
+		File:  spec.File,
+		Start: spec.Start,
+		Stop:  spec.Stop,
+	}, nil
+}
+
+func (s *FromEbpfProcedureSpec) Kind() plan.ProcedureKind {
+	return fromEbpfKind
+}
+
+func (s *FromEbpfProcedureSpec) Copy() plan.ProcedureSpec {
+	ns := new(FromEbpfProcedureSpec)
+
+	return ns
+}
+
+func createFromEbpfSource(prSpec plan.ProcedureSpec, dsid execute.DatasetID, a execute.Administration) (execute.Source, error) {
+	spec, ok := prSpec.(*FromEbpfProcedureSpec)
+	if !ok {
+		return nil, fmt.Errorf("invalid spec type %T", prSpec)
+	}
+
+	s := ebpf.NewSource(a.Allocator())
+	s.File = spec.File
+
+	return &EbpfIterator{
+		source: s,
+		id:     dsid,
+	}, nil
+}
+
+func (c *EbpfIterator) Connect() error {
+	return nil
+}
+func (c *EbpfIterator) Fetch() (bool, error) {
+	return false, nil
+}
+func (c *EbpfIterator) Decode() (flux.Table, error) {
+	return nil, nil
+}
+
+type EbpfIterator struct {
+	source *generate.Source
+	id     execute.DatasetID
+	ts     []execute.Transformation
+}
+
+func (c *EbpfIterator) Do(f func(flux.Table) error) error {
+	c.source.Connect()
+
+	more, err := c.source.Fetch()
+	if err != nil {
+		return err
+	}
+	for more {
+		tbl, err := c.source.Decode()
+		if err != nil {
+			return err
+		}
+		f(tbl)
+		more, err = c.source.Fetch()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *EbpfIterator) AddTransformation(t execute.Transformation) {
+	c.ts = append(c.ts, t)
+}
+
+func (c *EbpfIterator) Run(ctx context.Context) {
+	var err error
+	var max execute.Time
+	maxSet := false
+	err = c.Do(func(tbl flux.Table) error {
+		for _, t := range c.ts {
+			err := t.Process(c.id, tbl)
+			if err != nil {
+				return err
+			}
+			if idx := execute.ColIdx(execute.DefaultStopColLabel, tbl.Key().Cols()); idx >= 0 {
+				if stop := tbl.Key().ValueTime(idx); !maxSet || stop > max {
+					max = stop
+					maxSet = true
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		goto FINISH
+	}
+
+	if maxSet {
+		for _, t := range c.ts {
+			t.UpdateWatermark(c.id, max)
+		}
+	}
+
+FINISH:
+	for _, t := range c.ts {
+		t.Finish(c.id, err)
+	}
+}
