f193370f0f3c516b10622c985516ed66bc963e43
Adam <adam@influxdata.com>
2018-09-19 14:34:15 -0700

    refactored to from_iterator.go, needs some cleanup after hackathon

--- functions/from_iterator.go
+++ functions/from_iterator.go
@@ -28,7 +28,7 @@ func init() {
 	flux.RegisterFunction(FromTemplateKind, createFromTemplateOpSpec, fromTemplateSignature)
 	flux.RegisterOpSpec(FromTemplateKind, newFromTemplateOp)
 	plan.RegisterProcedureSpec(FromTemplateKind, newFromTemplateProcedure, FromTemplateKind)
-	execute.RegisterSource(FromTemplateKind, createFromTemplateSource)
+	execute.RegisterSource(FromTemplateKind, createFromSourceIterator)
 }
 
 func createFromTemplateOpSpec(args flux.Arguments, a *flux.Administration) (flux.OperationSpec, error) {
@@ -73,48 +73,50 @@ func (s *FromTemplateProcedureSpec) Copy() plan.ProcedureSpec {
 	return ns
 }
 
-func createFromTemplateSource(prSpec plan.ProcedureSpec, dsid execute.DatasetID, a execute.Administration) (execute.Source, error) {
-	// TODO: copy over info from the ProcedureSpec you need to run your source..
-	//spec, ok := prSpec.(*FromTemplateProcedureSpec)
-	//if !ok {
-	//	return nil, fmt.Errorf("invalid spec type %T", prSpec)
-	//}
-
-	return &TemplateIterator{}, nil
+type SourceDecoder interface {
+	Connect() error
+	Fetch() (bool, error)
+	Decode() (flux.Table, error)
 }
 
-func (c *TemplateIterator) Connect() error {
+func createFromSourceIterator(decoder SourceDecoder, dsid execute.DatasetID, a execute.Administration) (execute.Source, error) {
+	return &SourceIterator{decoder: decoder, id: dsid}, nil
+}
+
+
+// TODO: scrub these out of the source interface.
+func (c *SourceIterator) Connect() error {
 	return nil
 }
-func (c *TemplateIterator) Fetch() (bool, error) {
+func (c *SourceIterator) Fetch() (bool, error) {
 	return false, nil
 }
-func (c *TemplateIterator) Decode() (flux.Table, error) {
+func (c *SourceIterator) Decode() (flux.Table, error) {
 	return nil, nil
 }
 
 
-type TemplateIterator struct {
+type SourceIterator struct {
 	// TODO: add fields you need to connect, fetch, etc.
-
+	decoder SourceDecoder
 	id   execute.DatasetID
 	ts   []execute.Transformation
 }
 
-func (c *TemplateIterator) Do(f func(flux.Table) error) error {
+func (c *SourceIterator) Do(f func(flux.Table) error) error {
 	c.Connect()
 
-	more, err := c.Fetch()
+	more, err := c.decoder.Fetch()
 	if err != nil {
 		return err
 	}
 	for more {
-		tbl, err := c.Decode()
+		tbl, err := c.decoder.Decode()
 		if err != nil {
 			return err
 		}
 		f(tbl)
-		more, err = c.Fetch()
+		more, err = c.decoder.Fetch()
 		if err != nil {
 			return err
 		}
@@ -125,11 +127,11 @@ func (c *TemplateIterator) Do(f func(flux.Table) error) error {
 
 
 
-func (c *TemplateIterator) AddTransformation(t execute.Transformation) {
+func (c *SourceIterator) AddTransformation(t execute.Transformation) {
 	c.ts = append(c.ts, t)
 }
 
-func (c *TemplateIterator) Run(ctx context.Context) {
+func (c *SourceIterator) Run(ctx context.Context) {
 	var err error
 	var max execute.Time
 	maxSet := false
